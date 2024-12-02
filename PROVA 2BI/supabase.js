9d75fdbdf0434c5c1c48f977462bffcde1ef6679
Diogo Nest <111660044+diogonest@users.noreply.github.com>
2024-12-02 08:31:04 -0300

    Update supabase.js

--- PROVA 2BI/supabase.js
+++ PROVA 2BI/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://ksgekwqwgoxbokkeiack.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzZ2Vrd3F3Z294Ym9ra2VpYWNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMxMzcxNTcsImV4cCI6MjA0ODcxMzE1N30.T1saHUS360V4ogNgMPhKfD3Vxa1_XmcsGVzZaUpEcwk'
+const supabaseKey
 export const supabase = createClient(supabaseUrl, supabaseKey)
